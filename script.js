6cbf23ec78f04ff309eff36622486ccde771bf85
Gerzcantcode <164610278+Gerzcantcode@users.noreply.github.com>
2024-03-23 21:10:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+function gs(){
+    document.getElementById("gs").classList.toggle("active");
+}
+
+function jc(){
+    document.getElementById("jc").classList.toggle("active");
+}
+
+function afs(){
+    document.getElementById("afs").classList.toggle("active");
+}
+
+function ae(){
+    document.getElementById("ae").classList.toggle("active");
+}
+function pf(){
+    document.getElementById("pf").classList.toggle("active");
+}
+function kf(){
+    document.getElementById("kf").classList.toggle("active");
+}
+function qj(){
+    document.getElementById("qj").classList.toggle("active");
+}
+function ms(){
+    document.getElementById("ms").classList.toggle("active");
+}
+function as(){
+    document.getElementById("as").classList.toggle("active");
+}
+function js(){
+    document.getElementById("js").classList.toggle("active");
+}
+function pj(){
+    document.getElementById("pj").classList.toggle("active");
+}
+function pb(){
+    document.getElementById("pb").classList.toggle("active");
+}
+function am(){
+    document.getElementById("am").classList.toggle("active");
+}
+function km(){
+    document.getElementById("km").classList.toggle("active");
+}
+function jt(){
+    document.getElementById("jt").classList.toggle("active");
+}
+function me(){
+    document.getElementById("me").classList.toggle("active");
+}
+function sp(){
+    document.getElementById("sp").classList.toggle("active");
+}
+function se(){
+    document.getElementById("se").classList.toggle("active");
+}
+function jtp(){
+    document.getElementById("jtp").classList.toggle("active");
+    document.getElementById("jt").classList.toggle("active");
+}
+
+function jtpx(){
+    document.getElementById("jtp").classList.toggle("active");
+}
+
+function introduction(){
+    document.getElementById("in").classList.toggle("active");
+}
